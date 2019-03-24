75c91b081334178629520cd7e80233687f66612b
Mateusz Boczarski <matbocz@outlook.com>
2019-03-24 11:28:42 +0100

    Create oracle_bokser.sql

---
+++ oracle_bokser.sql
@@ -0,0 +1,49 @@
+-- 18560 Mateusz Boczarski
+
+-- Ponizej wklej poprawiony skrypt SQL dla systemu Oracle generujacy strukture bazy danych "bokser":
+
+
+-- Dopisz przykladowe rekordy z walkami (dodaj co najmniej 20 walk):
+
+
+-- 1. Wyswietlic imie, nazwisko, wage najciezszego zawodnika 
+--    oraz nazwe klubu, w ktorym trenuje.
+
+
+-- 2. Wypisac pieciu zawodnikow o najwiekszych wagach. 
+--    Jezeli kilku ma taka sama wage, wypisac tych o 
+--    nazwiskach wczesniejszych alfabetycznie.
+
+
+-- 3. Dla kazdego z klubow wypisac jego nazwe oraz 
+--    nazwisko i wage najlzejszego i najciezszego zawodnika.
+
+
+-- 4. Wypisac nazwiska bokserow, ktorzy nie przegrali 
+--    zadnej walki.
+
+
+-- 5. Wypisac nazwiska bokserow z liczba ich zwyciestw, 
+--    w kazdej startowanej kategorii wagowej.
+
+
+-- 6. Wypisac nazwy klubow z liczba zwyciestw ich zawodnikow.
+
+
+-- 7. Dla kazdego klubu wypisac maksymalna liczbe zwyciestw 
+--    jednego zawodnika tego klubu.
+
+
+-- 8. Wypisac kategorie wagowe, w ktorych nie wystepowal 
+--    zaden bokser.
+
+
+-- 9. Wypisac nazwy klubow, w ktorych nikt nie walczy.
+ 
+
+-- 10. Wypisac tych zawodnikow, dla ktorych imiona i nazwiska 
+--     sa takie same, ale wartosci indentyfikatorow sa rozne.
+
+
+-- 11. Wypisac nazwy klubow, ktorych zawodnicy wygrali 
+--     ponad 20% wszystkich walk.
